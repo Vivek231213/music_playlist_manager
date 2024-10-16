7f81d9262f9393efec8c53c97c187cfbd844c84d
Vivek231213 <vivekvivjnv1@gmail.com>
2024-10-17 00:32:16 +0530

    music playlist manager a rust project

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::io::{self, Write};
+use serde::{Serialize, Deserialize};
 
-// Song struct
-#[derive(Debug, Serialize, Deserialize, Clone)]
+// Song struct without album and with f32 duration
+#[derive(Serialize, Deserialize, Debug)]
 struct Song {
     title: String,
-    singer: String, // Changed from artist to singer
-    album: String,
-    duration: u32,
+    singer: String,
+    duration: f32,  
 }
 
-// Playlist struct
-#[derive(Debug, Serialize, Deserialize)]
+// Playlist struct to hold a list of songs
+#[derive(Serialize, Deserialize, Debug)]
 struct Playlist {
     name: String,
     songs: Vec<Song>,
 }
 
-// PlaylistManager struct
-#[derive(Debug, Serialize, Deserialize)]
+
+
+
+// PlaylistManager struct to manage multiple playlists
+#[derive(Serialize, Deserialize, Debug)]
 struct PlaylistManager {
     playlists: HashMap<String, Playlist>,
 }
 
+
+
+
 impl PlaylistManager {
-    // Constructor for a new PlaylistManager
+    // Initialize a new PlaylistManager
     fn new() -> Self {
         PlaylistManager {
             playlists: HashMap::new(),
@@ -34,48 +39,90 @@ impl PlaylistManager {
     }
 
     // Add a new playlist
-    fn add_playlist(&mut self, name: String) {
-        let playlist = Playlist {
-            name: name.clone(),
-            songs: Vec::new(),
-        };
-        self.playlists.insert(name, playlist);
-        println!("Playlist created!");
+    fn add_playlist(&mut self) {
+        let mut playlist_name = String::new();
+        println!("Enter playlist name:");
+
+        io::stdin().read_line(&mut playlist_name).expect("Failed to read line");
+        let playlist_name = playlist_name.trim().to_string();
+
+        if self.playlists.contains_key(&playlist_name) {
+            println!("Playlist '{}' already exists!", playlist_name);
+        } else {
+            let playlist = Playlist {
+                name: playlist_name.clone(),
+                songs: Vec::new(),
+            };
+            self.playlists.insert(playlist_name.clone(), playlist);
+            println!("Playlist '{}' added!", playlist_name);
+        }
     }
 
-    // Add a song to a playlist
-    fn add_song(&mut self, playlist_name: &str, song: Song) {
-        if let Some(playlist) = self.playlists.get_mut(playlist_name) {
+    // Add a new song to an existing playlist
+    fn add_song(&mut self) {
+
+        // Show all playlists
+        self.show_playlists();
+
+        let mut playlist_name = String::new();
+        println!("Enter playlist name to add song to:");
+        io::stdin().read_line(&mut playlist_name).expect("Failed to read line");
+        let playlist_name = playlist_name.trim().to_string();
+
+        if let Some(playlist) = self.playlists.get_mut(&playlist_name) {
+            let mut song_title = String::new();
+            let mut song_singer = String::new();
+            let mut song_duration = String::new();
+
+            println!("Enter song title:");
+            io::stdin().read_line(&mut song_title).expect("Failed to read line");
+
+            println!("Enter singer name:");
+            io::stdin().read_line(&mut song_singer).expect("Failed to read line");
+
+            println!("Enter song duration (in minutes and seconds):");
+            io::stdin().read_line(&mut song_duration).expect("Failed to read line");
+
+            let song_duration: f32 = song_duration.trim().parse().expect("Invalid duration input");
+
+            let song = Song {
+                title: song_title.trim().to_string(),
+                singer: song_singer.trim().to_string(),
+                duration: song_duration,
+            };
+
             playlist.songs.push(song);
-            println!("Song added to playlist!");
+            println!("Song added to playlist '{}'", playlist_name);
         } else {
-            println!("Playlist not found!");
+            println!("Playlist '{}' not found!", playlist_name);
         }
     }
 
-    // List all playlists
-    fn list_playlists(&self) {
-        if self.playlists.is_empty() {
-            println!("No playlists available.");
-        } else {
-            for (name, _) in &self.playlists {
-                println!("Playlist: {}", name);
-            }
+
+
+
+    // Display all playlists
+    fn show_playlists(&self) {
+        println!("Available playlists:");
+        for p_name in self.playlists.keys() {
+            println!("- {}", p_name);
         }
     }
 
-    // List songs in a playlist
-    fn list_songs(&self, playlist_name: &str) {
-        if let Some(playlist) = self.playlists.get(playlist_name) {
-            println!("Songs in playlist {}:", playlist_name);
+    // Display all songs in a playlist
+    fn show_songs_in_playlist(&self) {
+        let mut playlist_name = String::new();
+        println!("Enter playlist name to view songs:");
+        io::stdin().read_line(&mut playlist_name).expect("Failed to read line");
+        let playlist_name = playlist_name.trim().to_string();
+
+        if let Some(playlist) = self.playlists.get(&playlist_name) {
+            println!("Songs in playlist '{}':", playlist_name);
             for song in &playlist.songs {
-                println!(
-                    "Title: {}, Singer: {}, Album: {}, Duration: {}s",
-                    song.title, song.singer, song.album, song.duration
-                );
+                println!("Title: {}, Singer: {}, Duration: {} minuts", song.title, song.singer, song.duration);
             }
         } else {
-            println!("Playlist not found!");
+            println!("Playlist '{}' not found!", playlist_name);
         }
     }
 
@@ -98,82 +145,32 @@ impl PlaylistManager {
 
 fn main() {
     let mut manager = PlaylistManager::load();
+
     loop {
         println!("\n--- Music Playlist Manager ---");
-        println!("1. New playlist");
-        println!("2. Add song");
-        println!("3. Show playlists");
-        println!("4. Show songs");
-        println!("5. Save & Exit");
+        println!("1. Add Playlist");
+        println!("2. Add Song to Playlist");
+        println!("3. Show All Playlists");
+        println!("4. Show Songs in Playlist");
+        println!("5. Save and Exit");
 
-        print!("Your choice: ");
+        print!("Enter your choice: ");
         io::stdout().flush().unwrap();
 
         let mut choice = String::new();
-        io::stdin().read_line(&mut choice).unwrap();
+        io::stdin().read_line(&mut choice).expect("Failed to read input");
         let choice = choice.trim();
 
         match choice {
-            "1" => {
-                print!("Playlist name: ");
-                io::stdout().flush().unwrap();
-                let mut name = String::new();
-                io::stdin().read_line(&mut name).unwrap();
-                manager.add_playlist(name.trim().to_string());
-            }
-            "2" => {
-                // Show available playlists before adding a song
-                println!("Available playlists:");
-                manager.list_playlists();
-                
-                print!("Choose a playlist: ");
-                io::stdout().flush().unwrap();
-                let mut playlist_name = String::new();
-                io::stdin().read_line(&mut playlist_name).unwrap();
-
-                print!("Song title: ");
-                io::stdout().flush().unwrap();
-                let mut title = String::new();
-                io::stdin().read_line(&mut title).unwrap();
-
-                print!("Singer name: ");
-                io::stdout().flush().unwrap();
-                let mut singer = String::new();
-                io::stdin().read_line(&mut singer).unwrap();
-
-                print!("Song album: ");
-                io::stdout().flush().unwrap();
-                let mut album = String::new();
-                io::stdin().read_line(&mut album).unwrap();
-
-                print!("Song duration (seconds): ");
-                io::stdout().flush().unwrap();
-                let mut duration = String::new();
-                io::stdin().read_line(&mut duration).unwrap();
-
-                let song = Song {
-                    title: title.trim().to_string(),
-                    singer: singer.trim().to_string(),
-                    album: album.trim().to_string(),
-                    duration: duration.trim().parse().unwrap(),
-                };
-
-                manager.add_song(playlist_name.trim(), song);
-            }
-            "3" => manager.list_playlists(),
-            "4" => {
-                print!("Playlist name: ");
-                io::stdout().flush().unwrap();
-                let mut playlist_name = String::new();
-                io::stdin().read_line(&mut playlist_name).unwrap();
-                manager.list_songs(playlist_name.trim());
-            }
+            "1" => manager.add_playlist(),
+            "2" => manager.add_song(),
+            "3" => manager.show_playlists(),
+            "4" => manager.show_songs_in_playlist(),
             "5" => {
                 manager.save();
-                println!("Exiting... Goodbye!");
                 break;
             }
-            _ => println!("Invalid option! Try again."),
+            _ => println!("Invalid choice! Please enter a valid option."),
         }
     }
 }
